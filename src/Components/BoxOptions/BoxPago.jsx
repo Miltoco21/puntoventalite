57c72ed0927faceeb928a7093aa35c4add176f51
MiltonP <milton206@gmail.com>
2024-02-20 10:26:10 -0300

    botones metodo pago

--- src/Components/BoxOptions/BoxPago.jsx
+++ src/Components/BoxOptions/BoxPago.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useContext,useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import {
   Grid,
   Paper,
@@ -14,17 +14,14 @@ import {
   TextField,
   TableHead,
 } from "@mui/material";
-import { SelectedOptionsContext } from "../Context/SelectedOptionsProvider"
+import { SelectedOptionsContext } from "../Context/SelectedOptionsProvider";
 
 const RegistroCompra = () => {
-  const {
-    grandTotal
-
-  } = useContext(SelectedOptionsContext);
+  const { grandTotal } = useContext(SelectedOptionsContext);
 
   const [totalCompra, setTotalCompra] = useState(grandTotal);
   const [cantidadPagada, setCantidadPagada] = useState(0);
-
+  const [metodoPago, setMetodoPago] = useState("");
 
   // Función para calcular el monto restante por pagar
   const calcularPorPagar = () => {
@@ -38,13 +35,9 @@ const RegistroCompra = () => {
     setTotalCompra(total);
   };
 
-  // Manejar cambios en el campo de cantidad pagada
-  // const handleCantidadPagadaChange = (event) => {
-  //   const cantidad = parseFloat(event.target.value);
-  //   setCantidadPagada(cantidad);
-  //   setTotalCompra(grandTotal - cantidad);
-
-  // };
+  const handleMetodoPagoClick = (metodo) => {
+    setMetodoPago(metodo);
+  };
   const handleCantidadPagadaChange = (event) => {
     const cantidad = parseFloat(event.target.value);
     setCantidadPagada(cantidad);
@@ -68,43 +61,39 @@ const RegistroCompra = () => {
     }
   };
 
-
-
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Typography variant="h4">Registro de Compra</Typography>
+        <Typography variant="h4">Ventana de Pago</Typography>
       </Grid>
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} sm={6} md={6} lg={6}>
         <TextField
+          margin="dense"
           fullWidth
           type="number"
           label="Total de la compra"
           value={grandTotal}
           InputProps={{ readOnly: true }}
         />
-      </Grid>
-      <Grid item xs={12} sm={6}>
         <TextField
+          margin="dense"
           fullWidth
           type="number"
           label="Cantidad pagada"
           value={cantidadPagada}
           onChange={handleCantidadPagadaChange}
         />
-      </Grid>
-      <Grid item xs={12} sm={6}>
         <TextField
+          margin="dense"
           fullWidth
           type="number"
           label="Por pagar"
           value={calcularPorPagar()}
           InputProps={{ readOnly: true }}
         />
-      </Grid>
-      <Grid item xs={12} sm={6}>
         {calcularPorPagar() < grandTotal && (
           <TextField
+            margin="dense"
             fullWidth
             type="number"
             label="Vuelto"
@@ -114,11 +103,38 @@ const RegistroCompra = () => {
         )}
       </Grid>
 
-      <Grid>
-        <Button variant="outlined">Efectivo</Button>
-        <Button variant="outlined">Debito</Button>
-        <Button variant="outlined">Credito</Button>
+      <Grid item xs={12} md={6} lg={6}>
+        <Grid>
+          <Button
+            fullWidth
+            variant="outlined"
+            onClick={() => handleMetodoPagoClick("Efectivo")}
+          >
+            Efectivo
+          </Button>
+          <Button
+            fullWidth
+            variant="outlined"
+            onClick={() => handleMetodoPagoClick("Débito")}
+          >
+            Débito
+          </Button>
+          <Button
+            fullWidth
+            variant="outlined"
+            onClick={() => handleMetodoPagoClick("Crédito")}
+          >
+            Crédito
+          </Button>
+        </Grid>
       </Grid>
+      {metodoPago && (
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            Método de pago seleccionado: {metodoPago}
+          </Typography>
+        </Grid>
+      )}
     </Grid>
   );
 };
