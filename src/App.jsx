d84508cdcccd9d1e1cde97ef63e0747e2fb4e841
MiltonP <milton206@gmail.com>
2024-02-22 19:05:54 -0300

    login como pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes,Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
-import Login from "./Pages/Login"
-import Consolas from "./Pages/Consolas";
+import Login from "./Pages/Login";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -15,14 +14,14 @@ import { SelectedOptionsProvider } from "./Components/Context/SelectedOptionsPro
 function App() {
   return (
     <Router>
-    <SelectedOptionsProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </SelectedOptionsProvider>
-  </Router>
+      <SelectedOptionsProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </SelectedOptionsProvider>
+    </Router>
   );
 }
 
--- src/Components/BoxOptions/BoxDatosCaja.jsx
+++ src/Components/BoxOptions/BoxDatosCaja.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState, useContext,useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import {
   Paper,
   Box,
   Typography,
- 
   Grid,
   useTheme,
   Container,
@@ -14,33 +13,23 @@ import {
 } from "@mui/material";
 import { SelectedOptionsContext } from "../Context/SelectedOptionsProvider";
 
-
 const BoxVendedor = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const {
-    userData
+  const { userData } = useContext(SelectedOptionsContext);
 
-  } = useContext(SelectedOptionsContext);
+  console.log("userData context", userData);
 
-  console.log("userData context",userData)
-
-  
   const vendedores = [
     {
-      codigo: userData.codigoUsuario,
-      nombre:userData.nombres+" "+userData.apellidos,
+      codigo: userData.codigoUsuario || "21",
+      nombre: userData.nombres + " " + userData.apellidos ||" nombreapellido",
       caja: " 1",
-      rol:userData.rol,
-      boleta: " 3 1",
-      operacion: "Operación 1",
+      rol: userData.rol,
+      boleta: " 323232321",
+      operacion: "12123141",
     },
   ];
-
- 
-
-
-  
 
   const [vendedor, setVendedor] = useState([]);
 
@@ -63,36 +52,33 @@ const BoxVendedor = () => {
           sx={{
             marginBottom: "20px",
             padding: "20px",
-
           }}
         >
-          <Grid container item xs={12} spacing={2} alignItems="center">
-            <Grid item xs={6} md={6}
-           >
-              <TextField label="Vendedor:" value={vend.nombre} />
+          <Grid container item xs={12} lg={12} spacing={2} alignItems="center">
+            <Grid item xs={6} md={6} lg={6}>
+              <TextField fullWidth label="Vendedor:" value={vend.nombre} />
             </Grid>
-            <Grid item xs={3} md={3}label="Código">
-              <TextField  label="Código" value={vend.codigo} />
+            <Grid item xs={6} md={3} lg={6}>
+              <TextField 
+              fullWidth
+              label="Nº Última Operación" value={vend.operacion} />
             </Grid>
-            <Grid item xs={3} md={3}>
-              <TextField label="Caja" value={vend.caja} />
+            <Grid item xs={3} md={3} lg={3} label="Código">
+              <TextField fullWidth label="Código" value={vend.codigo} />
+            </Grid>
+            <Grid item xs={3} md={3} lg={2}>
+              <TextField fullWidth label="Caja" value={vend.caja} />
+            </Grid>
+
+            <Grid item xs={6} md={3} lg={6}>
+              <TextField fullWidth label="Última Boleta" value={vend.boleta} />
             </Grid>
            
-            <Grid item xs={6} md={3}>
-              <TextField label="Última Boleta" value={vend.boleta} />
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <TextField label="Nº Última Operación" value={vend.operacion} />
-            </Grid>
           </Grid>
         </Paper>
       ))}
     </>
   );
-
-  
 };
-
-
 
 export default BoxVendedor;
